010e04aa65ef59d0ac3e19b3fb9162713a0a43b5
Gerardo <gerardomisael9900@gmail.com>
2025-01-19 03:10:46 -0600

    Configuración para Render: host 0.0.0.0 y puerto dinámico

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from flask import Flask, render_template, request
 import sqlite3
+import os
 
 app = Flask(__name__)
 
@@ -26,5 +27,6 @@ def admin():
         pass
     return render_template('admin.html')
 
-if __name__ == '__main__':
-    app.run(debug=True)
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 5000))  # Usa el puerto asignado por Render
+    app.run(host="0.0.0.0", port=port)
